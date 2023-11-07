715b3e1f5c01c0bdd6621fc32c8ec0af1e5a5d8c
“Justyn” <justyntkw@gmail.com>
2023-11-07 11:01:58 +0800

    cpp ohnyo

---
+++ cpp/CPP Module 00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		return (0);
+	}
+
+	for (int i = 1; av[i]; i++)
+		for (int j = 0; av[i][j]; j++)
+			putchar(toupper(av[i][j]));
+	cout << endl;
+
+	return (0);
+}
