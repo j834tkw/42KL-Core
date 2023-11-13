17c5b7350494835a3e2aa03168e0e146415f60aa
“Justyn” <justyntkw@gmail.com>
2023-11-13 23:38:58 +0800

    i wanna play snowbreak pgr fest bye

--- cpp/CPP Module 01/ex04/main.cpp
+++ cpp/CPP Module 01/ex04/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstring>
 
 using std::string;
 using std::cout;
 using std::endl;
+using std::find;
+
+string rewriteString(string s1, string s2, string inFileLine)
+{
+	string	returnString;
+	size_t	foundPos = 0;
+	size_t	startWritePos = 0;
+	
+	foundPos = inFileLine.find(s1, 0);
+	while(foundPos != string::npos)
+	{
+		returnString.append(inFileLine.substr(startWritePos, (foundPos - startWritePos)));
+		returnString.append(s2);
+		startWritePos = foundPos + s1.length();
+		foundPos = inFileLine.find(s1, (foundPos + 1));
+	}
+	returnString.append(inFileLine.substr(startWritePos, inFileLine.length()));
+	return (returnString);
+}
 
 int main(int argc, char **argv)
 {
@@ -14,8 +34,34 @@ int main(int argc, char **argv)
 		return (0);
 	}
 
-	string s1 = argv[2];
-	string s2 = argv[3];
+	std::ifstream	inFile;
+	std::ofstream	outFile;
+	string			outFileName = argv[1];
+	string 			s1 = argv[2];
+	string 			s2 = argv[3];
+	string			inFileLine;
+
+	inFile.open(argv[1]);
+	if (!inFile.is_open())
+	{
+		cout << "Unable to open file" << endl;
+		return (-1);
+	}
+
+	outFileName.append(".replace");
+	outFile.open(outFileName.c_str());
+
+	while (1)
+	{
+		getline(inFile, inFileLine);
+		outFile << rewriteString(s1, s2, inFileLine);
+		if (inFile.eof())
+			break ;
+		outFile << "\n";
+	}
+
+	inFile.close();
+	outFile.close();
 
 	return (0);
 }
