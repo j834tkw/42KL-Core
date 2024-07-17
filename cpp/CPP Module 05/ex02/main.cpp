e6cc0568bd86c28d31bc7124aba9cc7fb03163b3
“Justyn” <justyntkw@gmail.com>
2024-07-17 15:20:41 +0800

    updated abit

--- cpp/CPP Module 05/ex02/main.cpp
+++ cpp/CPP Module 05/ex02/main.cpp
@@ -9,35 +9,35 @@ using std::endl;
 
 int main(void)
 {
-	// try
-	// {
-	// 	Bureaucrat B1("Crewmate", 1);
-	// 	PresidentialPardonForm F1("Imposter");
-	// 	PresidentialPardonForm F2(F1);
-	// 	B1.signForm(F2);
-	// 	B1.executeForm(F2);
-	// }
-	// catch (const std::exception& e) //catch any exception declared
-	// {
-	// 	cout << e.what() << endl;
-	// }
+	try
+	{
+		Bureaucrat B1("Crewmate", 1);
+		PresidentialPardonForm F1("Imposter");
+		PresidentialPardonForm F2(F1);
+		B1.signForm(F2);
+		B1.executeForm(F2);
+	}
+	catch (const std::exception& e) //catch any exception declared
+	{
+		cout << e.what() << endl;
+	}
 
 	cout << endl;
 
-	// try
-	// {
-	// 	Bureaucrat B2("NPC_0001", 145);
-	// 	Bureaucrat B3("Asimov", 1);
-	// 	RobotomyRequestForm F3("Noan");
-	// 	B2.signForm(F3); //cant sign form
-	// 	B3.signForm(F3);
-	// 	B2.executeForm(F3); //cant execute form
-	// 	B3.executeForm(F3);
-	// }
-	// catch (const std::exception& e)
-	// {
-	// 	cout << e.what() << endl;
-	// }
+	try
+	{
+		Bureaucrat B2("NPC_0001", 145);
+		Bureaucrat B3("Asimov", 1);
+		RobotomyRequestForm F3("Noan");
+		B2.signForm(F3); //cant sign form
+		B3.signForm(F3);
+		B2.executeForm(F3); //cant execute form
+		B3.executeForm(F3);
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+	}
 
 	cout << endl;
 
--- cpp/CPP Module 05/ex03/main.cpp
+++ cpp/CPP Module 05/ex03/main.cpp
@@ -17,7 +17,7 @@ int main(void)
 	try
 	{
 		Intern RandomDude3;
-		Bureaucrat B1("dark and black person", 1);
+		Bureaucrat B1("man", 1);
 		AForm *F1 = RandomDude3.makeForm("presidential pardon", "racist");
 
 		B1.signForm(*F1);
@@ -30,35 +30,35 @@ int main(void)
 
 	cout << endl;
 
-	// try
-	// {
-	// 	Intern RandomDude2;
-	// 	Bureaucrat B2("le babi oink oink", 1);
-	// 	AForm *F2 = RandomDude2.makeForm("robotomy request", "chicharron");
+	try
+	{
+		Intern RandomDude2;
+		Bureaucrat B2("yes", 1);
+		AForm *F2 = RandomDude2.makeForm("robotomy request", "robotto");
 
-	// 	B2.signForm(*F2);
-	// 	B2.executeForm(*F2);
-	// }
-	// catch (const std::exception& e)
-	// {
-	// 	cout << e.what() << endl;
-	// }
+		B2.signForm(*F2);
+		B2.executeForm(*F2);
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+	}
 
 	cout << endl;
 
-	// try
-	// {
-	// 	Intern RandomDude3;
-	// 	Bureaucrat B3("dunno", 1);
-	// 	AForm *F3 = RandomDude3.makeForm("shrubbery creation", "idk");
+	try
+	{
+		Intern RandomDude3;
+		Bureaucrat B3("dunno", 1);
+		AForm *F3 = RandomDude3.makeForm("shrubbery creation", "idk");
 
-	// 	B3.signForm(*F3);
-	// 	B3.executeForm(*F3);
-	// }
-	// catch (const std::exception& e)
-	// {
-	// 	cout << e.what() << endl;
-	// }
+		B3.signForm(*F3);
+		B3.executeForm(*F3);
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+	}
 
 	return (0);
 }
